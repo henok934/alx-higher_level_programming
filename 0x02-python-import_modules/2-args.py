0822ffaaa5091f8cef35bc595ea26aba19d1cd10
henok <teklemariammossie1@gmail.com>
2022-12-01 11:04:59 -0800

    using argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
+    import sys
 i = len(sys.argv) - 1
 if i == 0:
     print("{} arguments.".format(i))
@@ -13,4 +13,3 @@ for argv in sys.argv:
         i = 0
         print("{}: {}".format(i, argv))
         i = i + 1
-
