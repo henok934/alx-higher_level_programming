8c2a258d9953225608aa04f0e9ec74acde065f38
henok <teklemariammossie1@gmail.com>
2022-12-01 10:52:25 -0800

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+i = len(sys.argv) - 1
+if i == 0:
+    print("{} arguments.".format(i))
+elif i == 1:
+    print("{} argument:".format(i))
+else:
+    print("{} arguments:".format(i))
+for argv in sys.argv:
+    if i >= 1:
+        i = 0
+        print("{}: {}".format(i, argv))
+        i = i + 1
+
