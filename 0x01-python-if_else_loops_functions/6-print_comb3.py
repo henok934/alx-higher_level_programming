9299026a1a954f529d01a5bc955daf5e9d0eb891
henok <teklemariammossie1@gmail.com>
2022-11-30 18:16:44 -0800

    print number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 90):
-    print("{:02d}".format(i), end=", " if i < 89 else "\n")
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if (not (x == 8 and y == 9)):
+            print("{}{}".format(x, y), end=", ")
+        else:
+            print("{}{}".format(x, y))
