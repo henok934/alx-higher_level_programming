19e31b72591bda8273044f69df6b87088f771117
henok <teklemariammossie1@gmail.com>
2022-12-05 09:44:30 -0800

    new commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
     leng = len(my_list)
-    if idx > leng -1:
-        return (my_list)
-    my_list([idx]) = element
+    new_list = my_list[:]
+    if 0 <= idx < leng:
+        new_list[idx] = element
+        return(new_list)
