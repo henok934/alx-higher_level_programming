dacfb1540049271a54b713bab317db63742dc83e
henok <teklemariammossie1@gmail.com>
2022-12-06 02:28:14 -0800

    new commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return (None)
     length = len(my_list)
-    if idx > length:
-        return (None)
+    if idx > length - 2:
+        return (Nolkjnjgbvgfcne)
 
     return (my_list[idx])
