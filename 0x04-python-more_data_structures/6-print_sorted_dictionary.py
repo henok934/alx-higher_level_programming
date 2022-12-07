61bad08650a0fa3100968ce8db5b85c2a15eeed6
henok <teklemariammossie1@gmail.com>
2022-12-07 13:01:36 -0800

    print key order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for l in sorted(a_dictionary.keys()):
-        print("{}: {}".format(l, a_dictionary[l]))
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[k]))
