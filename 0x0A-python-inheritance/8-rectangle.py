ff4813f8e586874753b0db4c2fdb65ce71993151
henok <teklemariammossie1@gmail.com>
2023-01-10 03:45:15 -0800

    validate number

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """
 ===================================
 module with class Rectangle
 ===================================
 """
+B = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):
+class Rectangle(B):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
+        """using integer validator """
+
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
