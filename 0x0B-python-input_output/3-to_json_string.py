664a52d22f83319f614a2aac9946d20cc17ac161
henok <teklemariammossie1@gmail.com>
2023-01-12 11:26:09 -0800

    new commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """returns json representation of an object(string)"""
-from json import dumps as d
+import json
 
 
 def to_json_string(my_obj):
     """returns the json representation of an object"""
-    return d(my_obj)
-
+    data = json.dumps(my_obj)
+    return data
