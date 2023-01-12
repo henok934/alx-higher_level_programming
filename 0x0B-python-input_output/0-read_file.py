687ca02ad8453477e653cd63efa3440f187fefe3
henok <teklemariammossie1@gmail.com>
2023-01-12 01:36:29 -0800

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read().strip())
+        for i in f:
+            print(i, end="")
